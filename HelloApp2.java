f651b7c8ce6885e748116af8f2f617fe9c165931
yeshwanthgaddam17 <yeshwanthgaddam17@gmail.com>
2026-03-17 13:34:14 +0530

    UC2: Added greeting with command line argument

---
+++ HelloApp2.java
@@ -0,0 +1,9 @@
+public class HelloApp2 {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            System.out.println("Hello " + args[0]);
+        } else {
+            System.out.println("Hello User");
+        }
+    }
+}

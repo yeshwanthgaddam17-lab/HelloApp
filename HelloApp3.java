afeb323e388ddf7fc84248c9826b82cef8d42758
yeshwanthgaddam17 <yeshwanthgaddam17@gmail.com>
2026-04-04 20:35:32 +0530

    UC3: Display name with default 'World'

---
+++ HelloApp3.java
@@ -0,0 +1,16 @@
+public class HelloApp3 {
+    
+    public static void main(String[] args) {
+        String name;
+
+        if (args.length > 0) {
+            name = args[0];
+        } else {
+            name = "World";
+        }
+
+        System.out.println("Hello " + name);
+    }
+}
+    
+
